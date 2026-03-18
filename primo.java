c493798f97e2d277abf809bf8b1be36138c9698c
Maria Jose Callizaya Perez <callizayaperez@gmai.com>
2026-03-17 20:29:23 -0400

    Mejora algoritmo primo y actualización lista

--- primo.java
+++ primo.java
@@ -1,19 +1,18 @@
-public class primo {
-    public static void main(String[] args) {
-        int n = 7;
-        boolean esPrimo = true;
-
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                esPrimo = false;
-                break;
-            }
+public class Primo {
+    public static boolean esPrimo(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
         }
+        return true;
+    }
 
-        if (esPrimo) {
-            System.out.println("Es primo");
+    public static void main(String[] args) {
+        int num = 17;
+        if (esPrimo(num)) {
+            System.out.println(num + " es primo");
         } else {
-            System.out.println("No es primo");
+            System.out.println(num + " no es primo");
         }
     }
 }
