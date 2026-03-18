ac3ac5900baaa77a0afadb2f84fe9a75f0df12f0
Victor Villalobos <Joy7WolfLegenDary@gmail.com>
2026-03-17 20:47:41 -0400

    nueva version programa primo, edt, lista

--- primo.java
+++ primo.java
@@ -1,19 +1,39 @@
+import java.util.Scanner;
+
 public class primo {
     public static void main(String[] args) {
-        int n = 7;
-        boolean esPrimo = true;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingresa un numero: ");
+        int n = entrada.nextInt();
 
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                esPrimo = false;
-                break;
-            }
-        }
+        boolean esPrimo = esPrimoPorRaiz(n);
 
         if (esPrimo) {
             System.out.println("Es primo");
         } else {
             System.out.println("No es primo");
         }
+
+        entrada.close();
+    }
+
+    public static boolean esPrimoPorRaiz(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
